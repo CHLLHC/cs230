4acc154e0a7c1030d9389f345e1f128d484c55a0
CHL-Lenovo-VM <chl41993@gmail.com>
2017-01-26 10:48:39 -0800

    Phong_Shader good.

--- proj-rt/proj-rt-files/grading/phong_shader.cpp
+++ proj-rt/proj-rt-files/grading/phong_shader.cpp
@@ -12,22 +12,30 @@ vec3 Phong_Shader::Shade_Surface(const Ray& ray, const vec3& intersection_point,
 			cit != world.lights.end(); ++cit)
 	{
 		vec3 l = ((*cit)->position - intersection_point).normalized();
-		//vec3 r = -l + 2 * dot(l, same_side_normal) * same_side_normal;
 		//vec3 eDOTr = dot(-ray.direction, r);
 		double four_pi = 4 * 3.14159265358979323846264338327950288;
-		double Ld =
+		vec3 Ld =
 				((*cit)->brightness
 						/ (four_pi
-								* (intersection_point - (*cit)->position).magnitude_squared()));
+								* (intersection_point - (*cit)->position).magnitude_squared()))
+						* (*cit)->color;
 		double lDOTn = dot(l, same_side_normal);
 		vec3 Id;
 		if (lDOTn > 0)
 		{
-			Id = color_diffuse * Ld * (*cit)->color * lDOTn;
-			std::cout << color_diffuse << "," << Ld << "," << (*cit)->color
-					<< "," << lDOTn << "," << Id << std::endl;
+			Id = color_diffuse * Ld * lDOTn;
 		}
-		color += Id;
+
+		vec3 Ls = Ld;
+		vec3 r = -l + 2 * dot(l, same_side_normal) * same_side_normal;
+		double cos_phi = dot(r, -ray.direction)
+				/ (r.magnitude() * ray.direction.magnitude());
+		vec3 Is;
+		if (cos_phi > 0)
+		{
+			Is = color_specular * Ls * pow(cos_phi, specular_power);
+		}
+		color += Id + Is;
 	}
 	// TODO: determine the color
 	return color;
