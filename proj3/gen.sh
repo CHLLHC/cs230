9e9ed1bccc9ea8e564ef6f75e22c46ae66be89b6
CHL-Lenovo-VM <chl41993@gmail.com>
2017-03-16 18:59:53 -0700

    some data

---
+++ proj3/gen.sh
@@ -0,0 +1,2 @@
+g++ gendata.cpp -o gendata
+./gendata
---
+++ proj3/gendata.cpp
@@ -0,0 +1,16 @@
+/*
+ * gendata.cpp
+ *
+ *  Created on: Mar 16, 2017
+ *      Author: chl
+ */
+
+#include <iostream>
+
+int main() {
+	for (int i = 30;i<61;++i){
+		std::cout<<"cellp 120 "<<i<<" 100"<<std::endl;
+	}
+	return 0;
+}
+
